223e5c3f3df4ca2e0799124c20e48ff66de74b56
iameldon <eldon@eldon.com>
2024-01-20 20:51:32 +0800

    WheelPage Front End

--- src/Pages/WheelPage.js
+++ src/Pages/WheelPage.js
@@ -1,6 +1,115 @@
+// import "./styles.css";
+// import WheelComponent from "react-wheel-of-prizes";
+import { useState } from 'react'
+import '../Styles/style.css'
+
 const WheelPage = () => {
+    // const segments = [
+    //     "better luck next time",
+    //     "won 70",
+    //     "won 10",
+    //     "better luck next time",
+    //     "won 2",
+    //     "won uber pass"
+    //   ];
+    //   const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+    //   const onFinished = (winner) => {
+    //     console.log(winner);
+    //   };
+    // useState
+  const [wheelStyle, setWheelStyle] = useState("circle")
+
+  const startRotation = () => {
+    setWheelStyle("circle start-rotate");
+
+    setTimeout(() => {
+      setWheelStyle("circle start-rotate stop-rotate");
+    }, Math.floor(Math.random() * 10000) + 1);
+  }
+
     return (
-        <h1>Hello this is wheel page</h1>
+        <div>
+          <div className = "arrow"></div>
+          <ul className = {wheelStyle}>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">1
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">2
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">3
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">4
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">5
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">6
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">7
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">8
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">9
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">10
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">11
+              </div>
+            </li>
+            <li>
+              <div className = "text"
+                contentEditable="true"
+                spellCheck="false">12
+              </div>
+            </li>
+          </ul>
+          
+          <button className= "spin-button" onClick={startRotation}>
+            SPIN
+          </button>
+        </div>
+        
     )
 }
 
