18b08a07e6dd6153c32910a31007713a3f632ae0
Benjamin <benjaminlyseow@gmail.com>
2024-01-20 17:00:02 +0800

    trial commit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,18 @@
 const HomePage = () => {
+
+
     return (
-        <h1>Hello this is homepage</h1>
+        <header>
+            <h1>FeastFinder</h1>
+            <p>
+                <b>Cant decide what to eat?</b><br/>
+                Find your location, Select your preferences, Spin the wheel
+            </p>
+            <form>
+                <input/>
+                <button>Submit</button>
+            </form>
+        </header>
     )
 }
 
