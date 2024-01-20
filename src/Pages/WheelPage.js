31b5461bba036e757f5aa708f34168e7f18e1412
Chelsea-Chew <chewenzee@gmail.com>
2024-01-21 02:52:28 +0800

    Fixed some bugs

--- src/Pages/WheelPage.js
+++ src/Pages/WheelPage.js
@@ -1,22 +1,22 @@
 // import "./styles.css";
 // import WheelComponent from "react-wheel-of-prizes";
 import { useState } from 'react'
-import '../Styles/style.css'
+import '../Styles/WheelPage.css'
 
 const WheelPage = () => {
-    // const segments = [
-    //     "better luck next time",
-    //     "won 70",
-    //     "won 10",
-    //     "better luck next time",
-    //     "won 2",
-    //     "won uber pass"
-    //   ];
-    //   const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
-    //   const onFinished = (winner) => {
-    //     console.log(winner);
-    //   };
-    // useState
+  // const segments = [
+  //     "better luck next time",
+  //     "won 70",
+  //     "won 10",
+  //     "better luck next time",
+  //     "won 2",
+  //     "won uber pass"
+  //   ];
+  //   const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+  //   const onFinished = (winner) => {
+  //     console.log(winner);
+  //   };
+  // useState
   const [wheelStyle, setWheelStyle] = useState("circle")
 
   const startRotation = () => {
@@ -27,90 +27,90 @@ const WheelPage = () => {
     }, Math.floor(Math.random() * 10000) + 1);
   }
 
-    return (
-        <div>
-          <div className = "arrow"></div>
-          <ul className = {wheelStyle}>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">1
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">2
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">3
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">4
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">5
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">6
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">7
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">8
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">9
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">10
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">11
-              </div>
-            </li>
-            <li>
-              <div className = "text"
-                contentEditable="true"
-                spellCheck="false">12
-              </div>
-            </li>
-          </ul>
-          
-          <button className= "spin-button" onClick={startRotation}>
-            SPIN
-          </button>
-        </div>
-        
-    )
+  return (
+    <div>
+      <div className="arrow"></div>
+      <ul className={wheelStyle}>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">1
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">2
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">3
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">4
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">5
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">6
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">7
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">8
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">9
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">10
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">11
+          </div>
+        </li>
+        <li className="wheel-list">
+          <div className="text"
+            contentEditable="true"
+            spellCheck="false">12
+          </div>
+        </li>
+      </ul>
+
+      <button className="spin-button" onClick={startRotation}>
+        SPIN
+      </button>
+    </div>
+
+  )
 }
 
 export default WheelPage;
